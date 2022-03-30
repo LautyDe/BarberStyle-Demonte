b6ea076a7cbb4aa4495a19f300c8ff467134ebc5
LautyDe <lauty.d.p@gmail.com>
2022-03-30 19:31:26 -0300

    Comento la parte el logeo para continuarla en un futuro, y agrego lo que seria la estructura del carrito

--- entrega-clase4.js
+++ entrega-clase4.js
@@ -1,4 +1,4 @@
-const clave = 2324;
+/* const clave = 2324;
 
 function ingresarClave() {
   let claveIngresada = parseInt(prompt("Por favor, ingrese una clave:"));
@@ -17,4 +17,55 @@ function ingresarClave() {
 let claveAEvaluar;
 while (claveAEvaluar !== clave) {
   claveAEvaluar = ingresarClave();
+}
+ */
+
+class Carrito {
+  constructor(productos) {
+    this.productos = productos;
+  }
+  agregarProducto(producto) {
+    this.productos.push(producto);
+  }
+  verCarrito() {
+    console.log(this.productos);
+  }
+}
+
+class Producto {
+  constructor(nombre, precio) {
+    this.nombre = nombre;
+    this.precio = precio;
+  }
+}
+
+const tienda = new Carrito([]);
+
+mostrarMenu();
+
+function mostrarMenu() {
+  let op = "";
+  while (op !== "fin") {
+    op = prompt(`Ingrese una opcion (a, b o c)
+                        a. Agregar un producto
+                        b. Listar los productos
+                        c. fin`);
+    if (op === "a") {
+      const producto = crearProducto();
+      tienda.agregarProducto(producto);
+    } else if (op === "b") {
+      tienda.verCarrito();
+    } else if (op === "c") {
+      return alert("Gracias por su compra!");
+    } else {
+      alert("Opcion invalida");
+      return mostrarMenu();
+    }
+  }
+}
+
+function crearProducto() {
+  let nombre = prompt("Ingrese el nombre del producto: ");
+  let precio = prompt("ingrese el precio del producto: ");
+  return new Producto(nombre, precio);
 }
