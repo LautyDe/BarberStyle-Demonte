167eba8115f3a45634c6a7316b65ba45c9d0f355
LautyDe <lauty.d.p@gmail.com>
2022-04-06 22:57:22 -0300

    Guardo avances para poner botones desde JS

--- JS/carrito.js
+++ JS/carrito.js
@@ -1,1 +1,15 @@
-function shopBtn() {}
+function shopBtn() {
+  let plusButton = document.createElement("button");
+  plusButton.setAttribute("class", "plusButton");
+
+  let minusButton = document.createElement("button");
+  minusButton.setAttribute("class", "minusButton");
+
+  const buttons = document.getElementsByClassName("shopBtns");
+
+  document.getElementsByClassName("shopBtns");
+
+  document.body.appendChild(minusButton);
+}
+
+shopBtn();
