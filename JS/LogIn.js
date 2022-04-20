ae0ba437af77d26152f2834c26f4d1301b5dd1c2
LautyDe <lauty.d.p@gmail.com>
2022-04-20 20:16:49 -0300

    Cambio info de archivo de LogIn y elimino info repetida que tenia

---
+++ JS/LogIn.js
@@ -0,0 +1,21 @@
+/* const clave = 2324;
+
+function ingresarClave() {
+  let claveIngresada = parseInt(prompt("Por favor, ingrese una clave:"));
+  if (claveIngresada < 1000 || claveIngresada > 9999) {
+    alert("Su clave debe tener 4 digitos");
+  } else if (isNaN(claveIngresada)) {
+    alert("Debe ingresar un numero como clave");
+  } else if (claveIngresada != clave) {
+    alert("Clave incorrecta");
+  } else if (claveIngresada == clave) {
+    alert("Clave correcta!");
+  }
+  return claveIngresada;
+}
+
+let claveAEvaluar;
+while (claveAEvaluar !== clave) {
+  claveAEvaluar = ingresarClave();
+}
+ */
