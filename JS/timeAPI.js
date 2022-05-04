0d4e0f51ead7bf3c8509fc6a1baabcda068566f8
LautyDe <lauty.d.p@gmail.com>
2022-05-04 19:00:10 -0300

    guardo datetime en el header con fecha y hora

--- JS/timeAPI.js
+++ JS/timeAPI.js
@@ -1,14 +1,6 @@
-/* let url =
-  "https://www.timeapi.io/api/Time/current/zone?timeZone=America/Buenos_Aires";
+let url = "http://worldtimeapi.org/api/timezone/America/Argentina/Buenos_Aires";
 
-const options = {
-  mode: "no-cors",
-  headers: {
-    "Access-Control-Allow-Origin": "*",
-  },
-};
-
-fetch(url, options)
+fetch(url)
   .then((res) => res.json())
   .then((data) => {
     mostrarDatos(data);
@@ -17,6 +9,6 @@ fetch(url, options)
 function mostrarDatos(data) {
   const nodo = document.querySelector("#navbarNav");
   const div = document.createElement("div");
-  div.innerHTML = `${data.day}/${data.month}/${data.year}`;
+  div.innerHTML = `${data.datetime}`;
   nodo.appendChild(div);
-} */
+}
