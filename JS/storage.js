d0ec79904e64f253c249e63991745b437637cc42
LautyDe <lauty.d.p@gmail.com>
2022-05-17 21:26:11 -0300

    corrijo codigo para que se vean los productos en el carrito

--- JS/storage.js
+++ JS/storage.js
@@ -1,13 +1,22 @@
-let key = JSON.parse(localStorage.getItem("CARTS_KEY"));
+const key = JSON.parse(localStorage.getItem("CARTS_KEY"));
 
-if (!key || key.length == 0) {
-  const empty = document.querySelector("#items");
-  empty.innerHTML =
-    "<h3 class='empty'>Tu carrito está vacío</h3><br><h5 class='empty2'>¿No sabés qué comprar? ¡Ve al Shop para ver nuestra variedad de productos!</h5>";
-} else {
-  refreshCart();
+const container = document.querySelector("#items");
+function comprobarCarrito() {
+  if (!key || key.length == 0) {
+    empty.innerHTML = `<h3 class='empty'>Tu carrito está vacío</h3><br>
+     <h5class='empty2'>¿No sabés qué comprar? 
+     ¡Ve al Shop para ver nuestra variedad de productos!</h5> `;
+  } else {
+    refreshCart();
+  }
 }
-
+comprobarCarrito();
 function refreshCart() {
-  const full = document.querySelector("#items");
+  key.forEach((element) => {
+    const li = document.createElement("li");
+    li.innerHTML = `  
+        producto:${element.name}
+        precio:${element.price}`;
+    container.append(li);
+  });
 }
