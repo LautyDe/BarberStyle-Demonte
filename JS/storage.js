fb4237932482228b27b9e3e10f20d9026e0fc117
LautyDe <lauty.d.p@gmail.com>
2022-05-17 22:00:29 -0300

    corrijo parte del codigo cuando el carrito esta vacio para que aparezca bien el mensaje en cart.html

--- JS/storage.js
+++ JS/storage.js
@@ -3,8 +3,8 @@ const key = JSON.parse(localStorage.getItem("CARTS_KEY"));
 const container = document.querySelector("#items");
 function comprobarCarrito() {
   if (!key || key.length == 0) {
-    empty.innerHTML = `<h3 class='empty'>Tu carrito está vacío</h3><br>
-     <h5class='empty2'>¿No sabés qué comprar? 
+    container.innerHTML = `<h3 class='empty'>Tu carrito está vacío</h3><br>
+     <h5 class='empty2'>¿No sabés qué comprar? 
      ¡Ve al Shop para ver nuestra variedad de productos!</h5> `;
   } else {
     refreshCart();
